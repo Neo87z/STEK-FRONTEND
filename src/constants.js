4cb7d709fb53dd45923941661f6ffade74d2caf8
Neo87z <shehanhoradagoda12@gmail.com>
2022-11-13 14:28:04 -0330

    'main'

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ const PronounciationURL='http://0bb47bd4d2ed.ngrok.io'
 const EssayURL='http://0b1d-35-187-248-8.ngrok.io'
 const verbURl='http://72bd-34-75-39-190.ngrok.io'
 const GrammarURl='http://6fcb-104-198-34-29.ngrok.io'
-export {PronounciationURL,EssayURL,verbURl}
+export {PronounciationURL,EssayURL,verbURl,GrammarURl}
