e4103b918e680254292455e40e675df888eace8d
Neo87z <shehanhoradagoda12@gmail.com>
2022-11-13 22:39:48 -0330

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const PronounciationURL='http://3874730d0492.ngrok.io'
+const PronounciationURL='http://153798166669.ngrok.io'
 const EssayURL='http://b657-34-125-240-11.ngrok.io'
 const verbURl='http://1406-34-74-120-64.ngrok.io'
 const GrammarURl='http://be5e-34-68-37-10.ngrok.io'
